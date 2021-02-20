486022a59bf861e096b5427825350c65d3b4652a
yapoey_work <mohamed.medhat92@gmail.com>
2021-02-21 01:12:26 +0900

    Project Seed

---
+++ controllers/feed.js
@@ -0,0 +1,12 @@
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({ posts: [{ title: 'First Post', content: 'this is my first post' }] })
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: 'Post Created Successfully',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    })
+};
